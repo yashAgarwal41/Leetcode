e268f2874392b05edfb4058eab5e32d9871572fc
Yash Agarwal <agarwali4411@gmail.com>
2022-05-25 16:34:10 +0530

    Time: 541 ms (16.70%), Space: 10.5 MB (42.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    //Dp-Memoization
-    int dp[2501][2501];
-    int help(int i, int j, int &n, vector<int> &v)
-    {
-        if(i>=n)
-            return 0;
-        
-        if(j!=-1 and dp[i][j]!=-1)   return dp[i][j];
-        
-        int take = 0;
-        if(j==-1 or v[j]<v[i])    
-            take = help(i+1, i, n, v) + 1;
-        
-        int notTake = help(i+1, j, n, v);
-        
-        if(j==-1)   return max(take, notTake);
-        else
-            return dp[i][j] = max(take, notTake);
-        
-    }
+//Tabulation..TC-O(n^2), SC-O(n)
+//here dp[i] means, length of longest increasing subsequence(where v[i] is greatest ele of subsequence)    
+//     e.g- [5, 4, 11, 1, 16, 8]
+//           1  1  1   1   1  1     ->inital dp values
+//           1  1  2   1   3  2     ->final dp values
     int lengthOfLIS(vector<int>& v) {
-        int n=v.size();
-        memset(dp, -1, sizeof dp);
-        return help(0, -1, n, v);
+        int n = v.size();
+        vector<int> dp(n, 1);
+        
+        for(int i=1; i<n; i++)
+        {
+            for(int prev=0; prev<i; prev++)
+            {
+                if(v[prev]<v[i])
+                {
+                    dp[i] = max(dp[i], 1 + dp[prev]);
+                }
+            }
+        }
+        
+        return *max_element(dp.begin(), dp.end());
     }
 };
-
-
-
