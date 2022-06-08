b35727f4b1b10091cc8847326e8013849d3cbc53
Yash Agarwal <agarwali4411@gmail.com>
2022-06-08 15:19:03 +0530

    Time: 61 ms (37.08%), Space: 18.4 MB (34.91%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -10,53 +10,28 @@
  */
 class Solution {
 public:
-    //Storing (start, mid] elements in a queue..
-    //Storing [mid, end] elements in a stack..
-    //then interate in the linked list, and then reset its value with queue front value then stack top value
     void reorderList(ListNode* head) {
-        int n=0, i=0;
-        ListNode* temp = head, *start = head;
+        if(!head->next or !head->next->next)    return;
         
-        //Calcualting middle element..
+        ListNode* temp=head;
+        stack<ListNode*> st;
+        int n=0;
         while(temp)
         {
-            temp=temp->next;    n++;
-        }
-        ListNode* mid = head;
-        while(i++<n/2)  mid=mid->next;
-        
-        //Storing them in queue and stack respectively..
-        queue<int> q;
-        stack<int> st;
-        i=0;
-        while(i++<n/2)
-        {
-            q.push(start->val);
-            start = start->next;
-        }
-        while(mid)
-        {
-            st.push(mid->val);
-            mid=mid->next;
+            st.push(temp);
+            temp=temp->next;
+            n++;
         }
         
-        //now resetting the values in list..
-        temp=head;
-        while(!q.empty() or !st.empty())
+        ListNode* ptr = head;
+        for(int i=0; i<n/2; i++)
         {
-            if(!q.empty())
-            {
-                temp->val = q.front();
-                q.pop();
-                temp=temp->next;
-            }
-            if(!st.empty())
-            {
-                temp->val = st.top();
-                st.pop();
-                temp=temp->next;
-            }
+            ListNode * newNode = st.top();
+            st.pop();
+            newNode->next = ptr->next;
+            ptr->next = newNode;
+            ptr = newNode->next;
         }
-        
+        ptr->next = NULL;
     }
 };
