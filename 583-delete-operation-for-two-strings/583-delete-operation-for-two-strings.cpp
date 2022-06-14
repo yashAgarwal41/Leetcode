a8bd0b5d5b691a39a5df8982a48fca7dd39cfd02
Yash Agarwal <agarwali4411@gmail.com>
2022-06-14 11:57:54 +0530

    Time: 115 ms (12.86%), Space: 7.6 MB (61.15%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    //Exactly LCS question...TC-O(n^2)
     int dp[501][501];
-    int help(int i, int j, string &word1, string &word2)
+    int help(int i, int j, string &s1, string &s2)
     {
-        if(i<0 or j<0)  return 0;
+        if(i<0 or j<0)  return (i+1 + j+1);
         if(dp[i][j]!=-1)    return dp[i][j];
         
-        if(word1[i]==word2[j])  
-            return dp[i][j] = 1 + help(i-1, j-1, word1, word2);
-        else
-            return dp[i][j] = max(help(i-1, j, word1, word2), help(i, j-1, word1, word2));
+        if(s1[i]==s2[j])
+            return dp[i][j] = help(i-1, j-1, s1, s2);
+        else 
+            return dp[i][j] = 1 + min(help(i-1, j, s1, s2), help(i, j-1, s1, s2));
+        
     }
-    int minDistance(string word1, string word2) {
-        int n = word1.size(), m=word2.size();
+    int minDistance(string s1, string s2) {
+        int n =s1.size(), m=s2.size();
         memset(dp, -1, sizeof dp);
-        int common = help(n-1, m-1, word1, word2);
-        return n-common + m-common;
+        return help(n-1, m-1, s1, s2);
     }
 };
