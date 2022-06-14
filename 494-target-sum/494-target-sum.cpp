85cfe4716b0faf11460eafc2bc8d46e0b81323b2
Yash Agarwal <agarwali4411@gmail.com>
2022-06-14 16:41:29 +0530

    Time: 14 ms (77.36%), Space: 8.9 MB (93.76%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int dp[21][2005];
-    int help(int i, int sum, int &target, vector<int> &v)
+    int dp[21][1001];
+    int total=0;
+    int help(int i, int sum1, int &target, vector<int> &v)
     {
         if(i<0)
-            return sum==target;
+        {
+            int remaining = total - sum1;
+            if(sum1-remaining == target)    return 1;
+            else return 0;
+        }
+        if(dp[i][sum1]!=-1) return dp[i][sum1];
         
-        if(sum>=0 and dp[i][sum]!=-1)   return dp[i][sum];
-        if(sum<0 and dp[i][1000-sum]!=-1)   return dp[i][sum];
+        int pick = help(i-1, sum1+v[i], target, v);
+        int notPick = help(i-1, sum1, target, v);
         
-        int add = help(i-1, sum+v[i], target, v);
-        int sub = help(i-1, sum-v[i], target, v);
-        
-        if(sum<0)   return dp[i][1001 - sum] = add + sub;
-        else return dp[i][sum] = add + sub;
+        return dp[i][sum1] = pick + notPick;
     }
     int findTargetSumWays(vector<int>& v, int target) {
         int n=v.size();
+        for(int i=0; i<n; i++)  total+=v[i];
         memset(dp, -1, sizeof dp);
         return help(n-1, 0, target, v);
     }
