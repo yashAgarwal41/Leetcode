dea1386ada544bc0e2cc179122e9e2d652835b73
Yash Agarwal <agarwali4411@gmail.com>
2022-06-23 15:59:06 +0530

    Time: 82 ms (31.61%), Space: 32.1 MB (63.51%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
-//DFS..
-//start from index=start, and mark the visited cells. After dfs completes, check if element with value 0 is visited or not.. 
-    void dfs(int i, vector<int> &vis, vector<set<int>> &adj, vector<int> &v)
+    bool help(int i, vector<int> &v, vector<int> &vis)
     {
-        if(vis[i]==1)   return;
+        if(i<0 or i>=v.size())  return 0;
+        if(v[i]==0) return 1;
+        if(vis[i]==1)   return 0;
         vis[i]=1;
-        for(auto &j: adj[i])
-        {
-            if(!vis[j])
-            {
-                dfs(j, vis, adj, v);
-            }
-        }
+        bool left = help(i-v[i], v, vis);
+        bool right = help(i+v[i], v, vis);
+        return left or right;
     }
     bool canReach(vector<int>& v, int start) {
-        int n = v.size();
-        vector<int> vis(n, 0);
-        vector<set<int>> adj(n);
-        for(int i=0; i<n; i++)
-        {
-            int l = i-v[i];
-            int r = i+v[i];
-            if(l>=0) adj[i].insert(l);
-            if(r<n) adj[i].insert(r);
-        }
-
-        dfs(start, vis, adj, v);
-        for(int i=0; i<n; i++)
-        {
-            if(v[i]==0 and vis[i]==1)   return true; 
-        }
-        return false;
+        vector<int> vis(v.size(), 0);
+        return help(start, v, vis);
     }
 };
+
