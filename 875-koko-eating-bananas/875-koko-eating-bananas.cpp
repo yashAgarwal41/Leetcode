8a16ddd46ecc19323ccb5865d48e6cbc53937f1f
Yash Agarwal <agarwali4411@gmail.com>
2022-06-24 19:14:55 +0530

    Time: 64 ms (59.29%), Space: 18.9 MB (25.70%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    //Binary Search..
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        long long low = 1, high = 1e14, mini = INT_MAX;
+        while(low<=high)
+        {
+            long long mid = low + (high-low)/2;
+            long long cnt = 0;
+            for(auto &it : piles)
+            {
+                cnt += ceil((it*1.0)/mid);
+            }
+            
+            if(cnt <= h)
+            {
+                mini = min(mini, mid);
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return mini;
+    }
+};
