d04cc4a1af0e329724bf87c2cea591e5b0731d9c
Yash Agarwal <agarwali4411@gmail.com>
2022-06-25 15:44:12 +0530

    Time: 46 ms (64.81%), Space: 12.4 MB (58.72%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
+    //TC-O(n*m), SC-O(1)..Mark visited land as '2' in grid itself..
     int n, m;
-    void dfs(int i, int j, vector<vector<int>> &vis, vector<vector<char>> &grid)
+    void dfs(int i, int j, vector<vector<char>> &grid)
     {
-        if(i<0 or j<0 or i>=n or j>=m or grid[i][j]=='0' or vis[i][j]==1)  return;
-        vis[i][j]=1;
-        dfs(i-1, j, vis, grid);
-        dfs(i+1, j, vis, grid);
-        dfs(i, j-1, vis, grid);
-        dfs(i, j+1, vis, grid);
+        if(i<0 or j<0 or i>=n or j>=m or grid[i][j]=='0' or grid[i][j]=='0' or grid[i][j]=='2')  return;
+        grid[i][j]='2';
+        dfs(i-1, j, grid);
+        dfs(i+1, j, grid);
+        dfs(i, j-1, grid);
+        dfs(i, j+1, grid);
         return;
     }
     int numIslands(vector<vector<char>>& grid) {
         n = grid.size(), m=grid[0].size();
         int ans=0;
-        vector<vector<int>> vis(n, vector<int> (m, 0));
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
             {
-                if(grid[i][j]=='0') continue;
-                else if(vis[i][j]==0)
+                if(grid[i][j]=='1') //if land is not visited..
                 {
-                    dfs(i, j, vis, grid);
+                    dfs(i, j, grid);
                     ans++;
                 }
             }
