03ceee74cfacc9fd10429115a867e6dd96068aff
Yash Agarwal <agarwali4411@gmail.com>
2022-06-26 20:38:30 +0530

    Time: 16 ms (42.28%), Space: 13.8 MB (98.87%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     //DFS..
     int n, m, fix;
-    void dfs(int i, int j, int &color, vector<vector<int>> &vis, vector<vector<int>> &image)
+    void dfs(int i, int j, int &color, vector<vector<int>> &image)
     {
-        if(i<0 or j<0 or i>=n or j>=m or vis[i][j]==1 or image[i][j]!=fix)
+        if(i<0 or j<0 or i>=n or j>=m or image[i][j]!=fix)
             return;
         
-        vis[i][j] = 1;
         image[i][j] = color;
-        dfs(i-1, j, color, vis, image);
-        dfs(i+1, j, color, vis, image);
-        dfs(i, j-1, color, vis, image);
-        dfs(i, j+1, color, vis, image);
+        dfs(i-1, j, color, image);
+        dfs(i+1, j, color, image);
+        dfs(i, j-1, color, image);
+        dfs(i, j+1, color, image);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         n = image.size(), m = image[0].size(), fix = image[sr][sc];
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        dfs(sr, sc, color, vis, image);
+        
+        if(image[sr][sc]!=color)
+            dfs(sr, sc, color, image);
         return image;
     }
 };
