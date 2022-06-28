f18e4be98337313923f7e4d60a681fa6a4257ba1
Yash Agarwal <agarwali4411@gmail.com>
2022-06-28 12:29:39 +0530

    Time: 213 ms (66.69%), Space: 88.4 MB (34.87%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -4,34 +4,12 @@ public:
         int n = nums.size();
         int mini = *min_element(nums.begin(), nums.end());
         int maxi = *max_element(nums.begin(), nums.end());
-        int left=0, right=0, both=0, cnt=0;
-        for(int i=0; i<n; i++)
-        {
-            left++;
-            if(nums[i]==mini or nums[i]==maxi)
-                cnt++;
-            if(cnt==2)  break;
-        }
-        cnt=0;
-        for(int i=n-1; i>=0; i--)
-        {
-            right++;
-            if(nums[i]==mini or nums[i]==maxi)
-                cnt++;
-            if(cnt==2)  break;
-        }
-        for(int i=0; i<n; i++)
-        {
-            both++;
-            if(nums[i]==mini or nums[i]==maxi)
-                break;
-        }
-        for(int i=n-1; i>=0; i--)
-        {
-            both++;
-            if(nums[i]==mini or nums[i]==maxi)
-                break;
-        }
+        int i=find(nums.begin(), nums.end(), mini) - nums.begin();
+        int j=find(nums.begin(), nums.end(), maxi) - nums.begin();
+        int left = max(i, j) + 1;
+        int right = max(n-i, n-j);
+        int both = min(i, j)+1 + min(n-i, n-j);
+        
         return min({left, right, both});
     }
 };
