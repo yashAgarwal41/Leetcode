1edef801d079a7712dabefa5b60faf40fcc59554
Yash Agarwal <agarwali4411@gmail.com>
2022-07-01 17:31:55 +0530

    Time: 485 ms (10.30%), Space: 82.5 MB (5.33%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.cpp
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    //DFS..TC-O(n+e)..
+    //BFS..TC-O(n+e)..
     //Graph Bipartite (Coloring Algo)..
-    bool dfs(int i, vector<int> &color, vector<set<int>> &graph)
+    bool bfs(int i, vector<int> &color, vector<set<int>> &graph)
     {
-        if(color[i]==-1)    color[i]=0;
-        
-        for(auto &j:graph[i])
+        color[i] = 0;
+        queue<int> q;
+        q.push(i);
+        while(!q.empty())
         {
-            if(color[j]==-1)
+            int node = q.front();
+            q.pop();
+            
+            for(auto &j:graph[node])
             {
-                color[j] = 1^color[i];
-                if(dfs(j, color, graph)==false)
-                    return false;
+                 if(color[j]==-1)
+                 {
+                     color[j] = 1^color[node];
+                     q.push(j);
+                 }
+                 else if(color[j]==color[node])
+                     return false;
             }
-            else 
-            {
-                if(color[j]==color[i])
-                    return false;
-            }
-        }
+        }   
         return true;
-        
     }
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         vector<set<int>> graph(n+1);
@@ -31,12 +33,11 @@ public:
             graph[v[0]].insert(v[1]);
             graph[v[1]].insert(v[0]);
         }
-        
         for(int i=1; i<n; i++)
         {
             if(color[i]==-1)
             {
-                if(dfs(i, color, graph)==false)
+                if(bfs(i, color, graph)==false)
                     return false;
             }
         }
