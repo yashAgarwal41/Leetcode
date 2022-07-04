eab5ce869a5957dfc9a2444897cb3aef34b59f1d
Yash Agarwal <agarwali4411@gmail.com>
2022-07-04 13:18:15 +0530

    Time: 26 ms (77.35%), Space: 17.7 MB (84.26%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,63 +1,40 @@
 class Solution {
 public:
+    //Greedy..TC-O(2*n), SC-O(n)..
+    void distributeCandies(int &n, vector<int> &rat, vector<int> &candies)
+    {
+        for(int i=0; i<n; i++)
+        {
+            if(i>0 and i<n-1)
+            {
+                if(rat[i]>rat[i-1] and candies[i]<=candies[i-1]) 
+                    candies[i] = candies[i-1]+1;
+                else if(rat[i]>rat[i+1] and candies[i]<=candies[i+1])   
+                        candies[i]=candies[i+1]+1;
+            }
+            else if(i<n-1)  //at i=0..
+            {
+                if(rat[i]>rat[i+1] and candies[i]<=candies[i+1])
+                    candies[i]=candies[i+1]+1;
+                
+            }
+            else if(i>0)    //at i=n-1..
+            {
+                if(rat[i]>rat[i-1] and candies[i]<=candies[i-1])
+                    candies[i]=candies[i-1]+1;
+            }
+        }
+    }
     int candy(vector<int>& rat) {
         int n = rat.size();
-        vector<int> v(n, 1);
-        for(int i=0; i<n; i++)
-        {
-            if(i>0 and i<n-1)
-            {
-                if(rat[i]>rat[i-1] and v[i]<=v[i-1]) v[i] = v[i-1]+1;
-                else if(rat[i]>rat[i+1] and v[i]<=v[i+1])   
-                {
-                    // if(rat[i]==rat[i-1])    
-                        v[i]=v[i+1]+1;
-                    // else v[i] = max(v[i-1], v[i+1])+1;
-                }
-            }
-            else if(i<n-1)  //at i=0..
-            {
-                if(rat[i]>rat[i+1] and v[i]<=v[i+1])
-                {
-                    v[i]=v[i+1]+1;
-                }
-            }
-            else if(i>0)    //at i=n-1..
-            {
-                if(rat[i]>rat[i-1] and v[i]<=v[i-1])
-                    v[i]=v[i-1]+1;
-            }
-        }
+        vector<int> candies(n, 1);
+        
+        distributeCandies(n, rat, candies);   //first distribute from left to right
         reverse(rat.begin(), rat.end());
-        reverse(v.begin(), v.end());
-        for(int i=0; i<n; i++)
-        {
-            if(i>0 and i<n-1)
-            {
-                if(rat[i]>rat[i-1] and v[i]<=v[i-1]) v[i] = v[i-1]+1;
-                else if(rat[i]>rat[i+1] and v[i]<=v[i+1])   
-                {
-                    // if(rat[i]==rat[i-1])    
-                        v[i]=v[i+1]+1;
-                    // else v[i] = max(v[i-1], v[i+1])+1;
-                }
-            }
-            else if(i<n-1)  //at i=0..
-            {
-                if(rat[i]>rat[i+1] and v[i]<=v[i+1])
-                {
-                    v[i]=v[i+1]+1;
-                }
-            }
-            else if(i>0)    //at i=n-1..
-            {
-                if(rat[i]>rat[i-1] and v[i]<=v[i-1])
-                    v[i]=v[i-1]+1;
-            }
-        }
-        // for(auto it:v)  cout<<it<<" ";
-        // cout<<endl;
-        int sum = accumulate(v.begin(), v.end(), 0);
+        reverse(candies.begin(), candies.end());
+        distributeCandies(n, rat, candies);     //then check and distribute from right to left(i.e, reversed)
+        
+        int sum = accumulate(candies.begin(), candies.end(), 0);
         return sum;
     }
 };
