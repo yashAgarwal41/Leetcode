2b710977732e4ffa3fa76c099fdd7435163965a9
Yash Agarwal <agarwali4411@gmail.com>
2022-07-05 15:54:11 +0530

    Time: 987 ms (11.46%), Space: 142.5 MB (57.98%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.cpp
+++ 2055-plates-between-candles/2055-plates-between-candles.cpp
@@ -3,29 +3,28 @@ public:
     vector<int> platesBetweenCandles(string s, vector<vector<int>>& queries) {
         int n = s.size();
         vector<int> res;
-        vector<int> candies(n, 0), plates(n, 0);
-        candies[0] = (s[0]=='|');
+        vector<int> candies, plates(n, 0);
         plates[0] = (s[0]=='*');
-        
+        if(s[0]=='|')   candies.push_back(0);
         for(int i=1; i<n; i++)
         {
-            candies[i] = candies[i-1] + (s[i]=='|');
+            if(s[i]=='|')   candies.push_back(i);
             plates[i] = plates[i-1] + (s[i]=='*');
         }
-        for(auto &it : queries)
+        
+        for(auto &q:queries)
         {
-            int left = it[0], right= it[1];
-            int leftMost, rightMost;
-            if((left>0 and candies[left]>candies[left-1]) or (left==0 and candies[left]==1))
-                leftMost = left;
-            else leftMost = upper_bound(candies.begin(), candies.end(), candies[left]) - candies.begin();
+            int leftMost = lower_bound(candies.begin(), candies.end(), q[0]) - candies.begin();
+            int rightMost = upper_bound(candies.begin(), candies.end(), q[1]) - candies.begin()-1;
             
-            rightMost = lower_bound(candies.begin(), candies.end(), candies[right]) - candies.begin();
-            
-            if(leftMost >= right or rightMost<=left)    res.push_back(0);
-            else{
-               res.push_back(plates[rightMost] - plates[leftMost]);
+            if(leftMost >= rightMost)
+            {
+                res.push_back(0);
+                continue;
             }
+            
+            int leftInd = candies[leftMost], rightInd = candies[rightMost];
+            res.push_back(plates[rightInd] - plates[leftInd]);
         }
         
         return res;
