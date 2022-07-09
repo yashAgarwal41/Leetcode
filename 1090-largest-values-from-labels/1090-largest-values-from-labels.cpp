68d5bcc925e88910e84d7328704de3413fee0c6b
Yash Agarwal <agarwali4411@gmail.com>
2022-07-09 11:23:43 +0530

    Time: 48 ms (49.87%), Space: 20.3 MB (85.89%) - LeetHub

---
+++ 1090-largest-values-from-labels/1090-largest-values-from-labels.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int largestValsFromLabels(vector<int>& values, vector<int>& labels, int numWanted, int useLimit) {
+        int n = values.size();
+        vector<pair<int, int>> vp;
+        for(int i=0; i<n; i++)
+            vp.push_back({values[i], labels[i]});
+        
+        sort(vp.begin(), vp.end());
+        reverse(vp.begin(), vp.end());
+        
+        unordered_map<int, int> map;
+        int ans=0;
+        for(auto &it : vp)
+        {
+            // if(map.count(it.second))
+            // {
+                if(map[it.second]<useLimit)
+                {
+                    ans += it.first;
+                    map[it.second]++;
+                    numWanted--;
+                }
+            // }
+            // else 
+            // {
+            //     ans += it.first;
+            //     map[it.second]++;
+            //     numWanted--;
+            // }
+            if(numWanted==0)    break;
+        }
+        return ans;
+    }
+};
