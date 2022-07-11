689d46df0b22a16bb58666144c831defa6e9cdbb
Yash Agarwal <agarwali4411@gmail.com>
2022-07-11 11:49:05 +0530

    Time: 12 ms (10.18%), Space: 11.9 MB (66.66%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -14,17 +14,17 @@ public:
     //BFS- Level Order Traversal
     //Take last element of each level..
     vector<int> rightSideView(TreeNode* root) {
-        if(!root)   return {};
+        vector<int> res;
+        if(!root)   return res;
         queue<TreeNode*> q;
         q.push(root);
-        vector<int> res;
-        
+        TreeNode *node;
         while(!q.empty())
         {
             int n = q.size();
             for(int i=0; i<n; i++)
             {
-                TreeNode* node = q.front(); 
+                node = q.front(); 
                 q.pop();
                 if(node->left)  q.push(node->left);
                 if(node->right) q.push(node->right);
