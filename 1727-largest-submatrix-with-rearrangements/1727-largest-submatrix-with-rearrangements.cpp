c0702c8630870d66ba7bfe07129811c7e3fd9cde
Yash Agarwal <agarwali4411@gmail.com>
2022-07-15 17:18:50 +0530

    Time: 327 ms (49.08%), Space: 65.9 MB (82.41%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    //PrefixSum + Sorting..TC-O(n*m*log(m))..
-    void calculatePrefixSum(vector<vector<int>> &v, vector<vector<int>> &ps)
+    //PrefixSum + Sorting..TC-O(n*m*log(m)), SC-O(1)...
+    void calculatePrefixSum(vector<vector<int>> &v)
     {
         int n = v.size(), m = v[0].size();
         //Calculate no of consecutive 1's in every column.. 
@@ -12,23 +12,22 @@ public:
             {
                 sum += v[i][j];
                 if(v[i][j]==0)  sum=0;
-                ps[i][j] = sum;
+                v[i][j] = sum;  //storing prefixSum in given vector itself..
             }
         }
     }
     
     int largestSubmatrix(vector<vector<int>>& v) {
         int n = v.size(), m = v[0].size();
-        vector<vector<int>> ps(n, vector<int>(m, 0));
-        calculatePrefixSum(v, ps);
+        calculatePrefixSum(v);
         
         int maxi=0;
         for(int i=0; i<n; i++)
         {
-            sort(ps[i].begin(), ps[i].end());   //sorting every row..
+            sort(v[i].begin(), v[i].end());   //sorting every row..
             for(int j=0; j<m; j++)
             {
-                int area = ps[i][j]*(m-j);
+                int area = v[i][j]*(m-j);
                 maxi = max(maxi, area);
             }
         }
