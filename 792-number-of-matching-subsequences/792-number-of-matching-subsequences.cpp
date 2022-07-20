2672a5aa41613bb21ef6a5544d3b01997c45f991
Yash Agarwal <agarwali4411@gmail.com>
2022-07-20 10:56:19 +0530

    Time: 1925 ms (5.03%), Space: 155 MB (31.72%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
+    //
     int numMatchingSubseq(string s, vector<string>& words) {
         int n = s.size(), ans=0;
-        map<char, vector<int>> map;
+        map<char, set<int>> map;
         for(int i=0; i<n; i++)
         {
-            map[s[i]].push_back(i);
+            map[s[i]].insert(i);
         }
-        
-        for(int i=0; i<words.size(); i++)
+        // for(auto &it:map)
+        // {
+        //     cout<<it.first<<"-> ";
+        //     for(auto it2:it.second) cout<<it2<<" ";
+        //     cout<<endl;
+        // }
+        for(auto &str : words)
         {
-            string str = words[i];
-            int ind=0, ch=1;
-            for(int k=0; k<str.size(); k++)
+            int m = str.size(), j=-1;
+            bool found = true;
+            for(int i=0; i<str.size(); i++)
             {
-                auto it = map.find(str[k]);
-                if(it==map.end())   
+                if(!map.count(str[i]))
                 {
-                    ch=0;
+                    found = false;  
                     break;
                 }
-                int temp = lower_bound(it->second.begin(), it->second.end(), ind) - it->second.begin();
-                if(temp>=it->second.size())
+                auto ub = map[str[i]].upper_bound(j);
+                j = *ub;
+                if(ub == map[str[i]].end())
                 {
-                    ch=0;   break;
+                    found = false;
+                    break;
                 }
-                temp = it->second[temp];
-                ind = temp+1;
             }
-            if(ch)  
-            {
-                ans++;
-            }
+            if(found)   ans++;
         }
-        cout<<endl;
         return ans;
     }
 };
