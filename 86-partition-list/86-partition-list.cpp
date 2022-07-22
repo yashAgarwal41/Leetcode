3d4e4c356f3cd8aaee87f4f0239fa8e488679114
Yash Agarwal <agarwali4411@gmail.com>
2022-07-22 10:16:46 +0530

    Time: 3 ms (96.33%), Space: 10.3 MB (73.43%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -11,42 +11,26 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        if(head==NULL or head->next==NULL)  return head;
-        vector<int> small, large;
-        while(head!=NULL)
+        ListNode* smallHead = new ListNode(0);
+        ListNode* largeHead = new ListNode(0);
+        ListNode* newHead = smallHead, *temp = largeHead;
+        while(head)
         {
             int data = head->val;
-            if(data<x)  small.push_back(data);
-            else large.push_back(data);
-            head=head->next;
+            if(data<x)
+            {
+                smallHead->next = head;
+                smallHead = head;
+            }
+            else 
+            {
+                largeHead->next = head;
+                largeHead = head;
+            }
+            head = head->next;
         }
-        
-        ListNode* newHead = NULL, *temp=NULL;
-        if(small.size())    
-        {
-            ListNode *n = new ListNode(small[0]);
-            newHead = n;
-            temp = newHead;
-        }
-        else 
-        {
-            ListNode *n = new ListNode(large[0]);
-            newHead = n;
-            temp = newHead;
-        }
-        for(int i=1; i<small.size(); i++)
-        {
-            ListNode *n = new ListNode(small[i]);
-            temp->next = n;
-            temp = temp->next;
-        }
-        for(int i=0; i<large.size(); i++)
-        {
-            if(i==0 and newHead->val==large[0]) continue;
-            ListNode *n = new ListNode(large[i]);
-            temp->next = n;
-            temp = temp->next;
-        }
-        return newHead;
+        largeHead->next = NULL;
+        smallHead->next = temp->next;
+        return newHead->next;
     }
 };
