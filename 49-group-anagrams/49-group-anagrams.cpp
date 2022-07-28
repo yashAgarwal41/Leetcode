61104f8b77c9c8e90bc8da6fcbdaffabbe2e7647
Yash Agarwal <agarwali4411@gmail.com>
2022-07-28 17:03:43 +0530

    Time: 37 ms (90.31%), Space: 19.5 MB (82.66%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -3,32 +3,30 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n = strs.size();
         vector<pair<string, string>> dupc;
+        // dupc contains-> {sorted, actual} string[i] as a pair..
         for(int i=0; i<n; i++)
         {
-            string temp = strs[i];
-            sort(strs[i].begin(), strs[i].end());
-            dupc.push_back({strs[i], temp});
+            string orig = strs[i];  //original
+            sort(strs[i].begin(), strs[i].end());   //sorted
+            dupc.push_back({strs[i], orig});    
         }
-        // sort(strs.begin(), strs.end());
-        sort(dupc.begin(), dupc.end());
+        sort(dupc.begin(), dupc.end()); //now sort the 'dupc' and store all anagram groups..
         vector<vector<string>> res;
         string prev = "-";
         vector<string> ans;
         for(int i=0; i<n; i++)
         {
-            if(dupc[i].first==prev or prev == "-")
+            if(dupc[i].first == prev or prev == "-")
             {
-                // cout<<"hel";
                 prev = dupc[i].first;
-                ans.push_back(dupc[i].second);
+                ans.push_back(dupc[i].second);  //store the actual string..
             }
             else 
             {
-                // cout<<"byr";
                 res.push_back(ans);
                 ans.clear();
                 prev = dupc[i].first;
-                ans.push_back(dupc[i].second);
+                ans.push_back(dupc[i].second);  //store the actual string..
             }
         }
         if(ans.size())  res.push_back(ans);
