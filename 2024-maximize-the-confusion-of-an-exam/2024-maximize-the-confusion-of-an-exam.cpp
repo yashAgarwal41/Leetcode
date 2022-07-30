53f9dd53470183ca2e72b6bc5d45ba71f8764dd2
Yash Agarwal <agarwali4411@gmail.com>
2022-07-30 13:12:14 +0530

    Time: 44 ms (47.60%), Space: 10 MB (94.39%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     //Sliding Window..TC-O(n), SC-O(1)..
     // maxi = max(max 'T' together, max 'F' together)..
-    int maxConfuse(string &s, int k)
+    int maxConfuse(string &s, int k, char convert)
     {
         int maxi = 1, i = 0, j = 0, n = s.size();
         while(i<n and j<n)
         {
-            if(s[j]=='F')
+            if(s[j]==convert)
             {
                 k--;
                 while(k<0)
                 {
-                    k += s[i]=='F';
+                    k += s[i]==convert;
                     i++;
                 }
             }
@@ -25,17 +25,9 @@ public:
         int n = s.size();
         int maxi;
         //convert 'F' to 'T' and check maximum consecutives..
-        maxi = maxConfuse(s, k);    //1st call..
-        
         //convert 'T' to 'F' and check maximum consecutives..
-        //so to use the same function again, we just flipped the answers and then check for 'F' to 'T'..
-        for(auto &c : s)
-        {
-            if(c == 'T')    c='F';
-            else c = 'T';
-        }
         
-        maxi = max(maxi, maxConfuse(s, k)); //2nd call..
+        maxi = max(maxConfuse(s, k, 'F'), maxConfuse(s, k, 'T')); 
         return maxi;
     }
 };
