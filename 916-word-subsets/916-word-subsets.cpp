0b6c3c6733c34b0c113edad4c541abfc0e791826
Yash Agarwal <agarwali4411@gmail.com>
2022-07-30 10:20:37 +0530

    Time: 224 ms (80.68%), Space: 102.3 MB (73.71%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     //TC-O(n*26), SC-O(26)
-    bool isSubset(string &str, vector<int> freq)
+    void countMaxFreq(string &str2, vector<int> &freq)
     {
-        for(auto &c : str)
+        vector<int> individFreq(26, 0);
+        for(auto &c : str2)
         {
-            if(freq[c-'a']) 
+            individFreq[c-'a']++;
+        }
+        for(int i=0; i<26; i++)
+            freq[i] = max(freq[i], individFreq[i]);
+        
+    }
+    bool isSubset(string &str1, vector<int> freq)
+    {
+        for(auto &c : str1)
+        {
+            if(freq[c-'a']) //if that character exist in str, then decrease its freq..
                 freq[c-'a']--;
         }
         for(int i=0; i<26; i++)
@@ -16,21 +27,16 @@ public:
     }
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         vector<int> freq(26, 0);
-        for(auto &str : words2)
+        for(auto &str2 : words2)    //first calculate the maxFreq of letters in words2..
         {
-            vector<int> individFreq(26, 0);
-            for(auto &c : str)
-            {
-                individFreq[c-'a']++;
-            }
-            for(int i=0; i<26; i++)
-                freq[i] = max(freq[i], individFreq[i]);
+            countMaxFreq(str2, freq);   
         }
+        
         vector<string> res;
-        for(auto &str : words1)
+        for(auto &str1 : words1)
         {
-            if(isSubset(str, freq))
-                res.push_back(str);
+            if(isSubset(str1, freq))
+                res.push_back(str1);
         }
         return res;
     }
