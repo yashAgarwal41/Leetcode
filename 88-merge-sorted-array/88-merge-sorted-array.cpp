485d6e22d7bc18796116f057ea57b1150e7d3d02
Yash Agarwal <agarwali4411@gmail.com>
2022-08-04 01:38:48 +0530

    Time: 7 ms (28.12%), Space: 9.1 MB (72.00%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=m-1, j=n-1, k=n+m-1;
-        while(j>=0)
+    //TC-O(m+n), SC-O(1)..
+    void merge(vector<int>& v1, int m, vector<int>& v2, int n) {
+        int i=m-1, j=n-1, pos=m+n-1;
+        while(pos>=0)
         {
-            if(i>=0 and nums1[i]>=nums2[j])
+            if(i>=0 and j>=0)
             {
-                nums1[k] = nums1[i];
-                k--;    i--;
+                if(v1[i]<=v2[j])
+                    v1[pos--] = v2[j--];
+                else 
+                    v1[pos--] = v1[i--];
             }
+            else if(i>=0)
+                v1[pos--] = v1[i--];
             else 
-            {
-                nums1[k] = nums2[j];
-                k--;    j--;
-            }
+                v1[pos--] = v2[j--];
         }
-        
     }
 };
