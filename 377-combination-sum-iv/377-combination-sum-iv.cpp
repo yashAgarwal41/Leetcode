e6503cf3e4b1a828c51cca17a841179172724f0c
Yash Agarwal <agarwali4411@gmail.com>
2022-08-05 16:10:51 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (5.73%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    //Memoization..TC-O(n*target)
-    int n;
     int dp[201][1001];
-    int help(int i, int tar, vector<int> &v)
+    int help(int i, int target, vector<int> &v, int n)
     {
-        if(tar==0)  return 1;
-        if(i<0 or tar<0) return 0;
-        if(dp[i][tar]!=-1)  return dp[i][tar];
+        if(target==0)   return 1;
+        if(i<0 or target<0) return 0;
+        if(dp[i][target]!=-1)   return dp[i][target];
         
-        int take = 0;
-        if(tar>=v[i])   take = help(n-1, tar-v[i], v);
-        int notTake = help(i-1, tar, v);
+        int pick = 0;
+        if(target>=v[i]) pick = help(n-1, target-v[i], v, n);    //have to take all permutations
+        int notPick = help(i-1, target, v, n);
         
-        return dp[i][tar] = take + notTake;
+        return dp[i][target] = pick + notPick;
     }
     int combinationSum4(vector<int>& v, int target) {
-        n=v.size();
+        int n = v.size();
         memset(dp, -1, sizeof dp);
-        return help(n-1, target, v);
+        return help(n-1, target, v, n);
     }
 };
