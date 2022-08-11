d2b4ea69b1aec05746e17395425c700a71decfe1
Yash Agarwal <agarwali4411@gmail.com>
2022-08-11 12:34:35 +0530

    Time: 15 ms (37.73%), Space: 13.3 MB (24.47%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    //BFS..TC-O(n)..
+    //BFS..TC-O(n*m)..
     //cannot be solved using DFS(difficult to solve)..
     //as we have to move at each next level simultaneously, prefer BFS..
-    int n, m, maxTime = 0;
+    int n, m, maxTime = 0, cntRotted=0, cntFresh=0;
     int dRow[4] = {-1, 0, 0, 1};
     int dCol[4] = {0, -1, 1, 0};
     
@@ -23,8 +23,8 @@ public:
                 if(nRow>=0 and nRow<n and nCol>=0 and nCol<m and grid[nRow][nCol]==1 and vis[nRow][nCol]==0)
                 {
                     q.push({{nRow, nCol}, t+1});    //push the node with t+1..(1 minute to rotten the fresh orange)..
-                    grid[nRow][nCol] = 2;   //make it rotten
                     vis[nRow][nCol] = 1;    //visited
+                    cntRotted++;
                 }
             }
         }
@@ -44,19 +44,14 @@ public:
                     q.push({{i, j}, 0});
                     vis[i][j] = 1;  //mark as visited..
                 }
+                else if(grid[i][j]==1)  cntFresh++;
             }
         }
         
         bfs(q, grid, vis);  //call BFS..
         
         //if some fresh orange renains, return -1..
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<m; j++)
-            {
-                if(grid[i][j] == 1) return -1;
-            }
-        }
+        if(cntRotted != cntFresh)   return -1;
         return maxTime;
     }
 };
