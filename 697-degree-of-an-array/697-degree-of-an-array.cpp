8dc5627645c8ee0c9077ba042ca1c903f775dfa2
Yash Agarwal <agarwali4411@gmail.com>
2022-08-18 17:40:29 +0530

    Time: 42 ms (93.59%), Space: 27.8 MB (18.34%) - LeetHub

--- 697-degree-of-an-array/697-degree-of-an-array.cpp
+++ 697-degree-of-an-array/697-degree-of-an-array.cpp
@@ -3,39 +3,20 @@ public:
     //Calculate degree, then elements that have degree and the starting and ending point of those..TC-O(n)
     int findShortestSubArray(vector<int>& nums) {
         int n = nums.size(), degree=0;
-        int maxi = *max_element(nums.begin(), nums.end());
-        unordered_map<int, int> freqmap;
-        for(auto &it:nums)
-        {
-            freqmap[it]++;
-            degree = max(degree, freqmap[it]);
-        }
-        
-        vector<int> degreeNums(maxi+1, 0);
-        for(auto &it:freqmap)
-        {
-            if(it.second==degree)
-                degreeNums[it.first] = 1;
-        }
-        
-        vector<int> start(maxi+1, -1), end(maxi+1, -1);
+        unordered_map<int, vector<int>> mmap;
         for(int i=0; i<n; i++)
         {
-            if(degreeNums[nums[i]] == 1 and start[nums[i]] == -1)
-                start[nums[i]] = i;
-        }
-        for(int i=n-1; i>=0; i--)
-        {
-            if(degreeNums[nums[i]] == 1 and end[nums[i]] == -1)
-                end[nums[i]] = i;
+            mmap[nums[i]].push_back(i);
+            int sz = mmap[nums[i]].size();
+            degree = max(degree, sz);
         }
         
         int shortest = INT_MAX;
-        for(int i=0; i<=maxi; i++)
+        for(auto &it:mmap)
         {
-            if(start[i] != -1)
+            if(it.second.size() == degree)
             {
-                shortest = min(shortest, end[i]-start[i]+1);
+                shortest = min(shortest, it.second.back() - it.second[0] + 1);
             }
         }
         return shortest;
