a3a3a564437702bc4b7efe4798df7798422bba33
Yash Agarwal <agarwali4411@gmail.com>
2022-08-23 16:54:56 +0530

    Time: 61 ms (10.79%), Space: 22.3 MB (91.54%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
-    //Xor..TC-O(n)
+    //Binary Search..TC-O(nlogn)
     int singleNonDuplicate(vector<int>& v) {
-        int x=0;
-        for(auto i:v)   x^=i;
-        return x;
+        int n = v.size();
+        int low=0, high = n-1;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            
+            if(mid>0 and v[mid] == v[mid-1])
+            {
+                if((mid+1)%2==0)    low = mid+1;
+                else high = mid-1;
+            }
+            else if(mid<n-1 and v[mid] == v[mid+1])
+            {
+                if((n-mid)%2==0)    high = mid-1;
+                else low = mid+1;
+            }
+            else return v[mid];
+        }
+        return v[low];
+        
     }
 };
