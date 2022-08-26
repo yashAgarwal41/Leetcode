17bf00a444f494bd428334857d2b1d0ae88a82dd
Yash Agarwal <agarwali4411@gmail.com>
2022-08-26 12:36:18 +0530

    Time: 6 ms (50.57%), Space: 6 MB (83.90%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
-    string sortInt(int n)
-    {
-        string s = to_string(n);
-        sort(s.begin(), s.end());
-        return s;
-    }
+    //TC-O(9*logn)
     bool reorderedPowerOf2(int n) {
-        
-        string s = sortInt(n);
-        for(int i=0; i<32; i++)
+        int freq[10] = {0};
+        while(n>0)
         {
-            string powOf2Str = sortInt(1 << i);
-            if(powOf2Str == s)  return true;
+            freq[n%10]++;
+            n/=10;
+        }
+        int p=0, x=1;
+        while(p<30)
+        {
+            int cnt[10]={0};
+            int temp = x;
+            while(temp>0)
+            {
+                cnt[temp%10]++;
+                temp/=10;
+            }
+            bool found=true;
+            for(int i=0; i<=9; i++)
+            {
+                if(freq[i] != cnt[i])
+                {
+                    found = false;
+                    break;
+                }
+            }
+            if(found)   return true;
+            
+            x*=2;
+            p++;
         }
         return false;
-        
     }
 };
