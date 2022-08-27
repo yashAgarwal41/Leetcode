9ec0a920e453afc9dda1b2a31a728ab05af96b4b
Yash Agarwal <agarwali4411@gmail.com>
2022-08-27 19:22:46 +0530

    Time: 3 ms (88.69%), Space: 11.1 MB (29.48%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -5,19 +5,30 @@ public:
         int low=0, high = n-1;
         while(low<=high)
         {
-            int mid = low + (high-low)/2;
-            if(v[mid]==target)  return mid;
-            if(v[mid]>=v[low])
+            int mid = low+(high-low)/2;
+            if(target==v[mid])  return mid;
+            if(v[mid]>=v[0])
             {
-                if(target<=v[mid] and target>=v[low])   high=mid-1;
+                if(target>=v[0] and target<=v[mid])
+                    high = mid-1;
                 else low=mid+1;
             }
             else 
             {
-                if(target>=v[mid] and target<=v[high])  low=mid+1;
-                else high=mid-1;
+                if(v[mid]<=v[n-1])
+                {
+                    if(target>v[mid] and target<=v[n-1])
+                        low=mid+1;
+                    else high=mid-1;
+                }
+                else 
+                {
+                    if(target>v[mid])   high=mid-1;
+                    else low=mid+1;
+                }
+                    
             }
-        }   
+        }
         return -1;
     }
 };
