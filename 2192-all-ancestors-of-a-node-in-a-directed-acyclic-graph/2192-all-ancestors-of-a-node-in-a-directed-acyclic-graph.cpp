8cf0cf7611ef900555b404fe90a6730350cad4a2
Yash Agarwal <agarwali4411@gmail.com>
2022-09-03 14:52:59 +0530

    Time: 620 ms (42.01%), Space: 207.2 MB (6.82%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,42 +1,33 @@
 class Solution {
 public:
-    //BFS, TopoSort
+    //DFS, TC-O(n^2)
+    //call dfs from every node and store nodes which can reachable by startNode(source)..
+    void dfs(int src, int i, vector<int> &vis, vector<vector<int>> &graph, vector<vector<int>> &ans)
+    {
+        vis[i] = 1;
+        ans[i][src] = 1;
+        for(auto &j:graph[i])
+        {
+            if(!vis[j])
+                dfs(src, j, vis, graph, ans);
+        }
+    }
+    
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         vector<vector<int>> graph(n);
-        vector<int> indegree(n, 0);
+        vector<vector<int>> ans(n, vector<int> (n, 0));
+        // ans[i][j] = 1, if 'j' can reach 'i'..
         for(auto it:edges)
         {
             graph[it[0]].push_back(it[1]);
-            indegree[it[1]]++;
+            ans[it[1]][it[0]] = 1;   //it1 is reachable by it0..
         }
         
-        queue<int> q;
-        for(int i=0; i<n; i++)  
+        // start dfs from every node..
+        for(int i=0; i<n; i++)
         {
-            if(indegree[i]==0)  q.push(i);
-        }
-        
-        
-        vector<vector<int>> ans(n, vector<int> (n, 0));
-        // ans[i][j] = 1, if 'j' can reach 'i'..
-        while(!q.empty())
-        {
-            int node = q.front();
-            q.pop();
-            for(auto &j : graph[node])
-            {
-                indegree[j]--;
-                if(indegree[j] == 0)    q.push(j);
-                
-                ans[j][node] = 1;   //'j' is reachable by 'node'
-                //insert all the incoming nodes of 'node' in ans[j]..
-                for(int x=0; x<n; x++)
-                {
-                    if(ans[node][x] == 1)   
-                        ans[j][x] = 1;
-                }
-                
-            }
+            vector<int> vis(n, 0);
+            dfs(i, i, vis, graph, ans);
         }
         
         vector<vector<int>> res(n);
@@ -44,6 +35,7 @@ public:
         {
             for(int j=0; j<n; j++)
             {
+                if(i==j)    continue;
                 if(ans[i][j]==1)    res[i].push_back(j);
             }
         }
