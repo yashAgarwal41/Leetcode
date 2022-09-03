c545b4ace80ef311286c0263bb9db8ff758f7879
Yash Agarwal <agarwali4411@gmail.com>
2022-09-03 14:39:13 +0530

    Time: 364 ms (65.61%), Space: 67.4 MB (51.08%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
-    //Kahn's Algo..BFS, TC-O(n*n)
+    //DFS, TC-O(n*n)
     //que[a, b] is true if 'b' is reachable by 'a'..
+    void dfs(int src, int i, vector<int> &vis, vector<vector<int>> &graph, vector<vector<int>> &ans)
+    {
+        vis[i] = 1;
+        ans[i][src] = 1;    //'src' can reach 'i'..
+        for(auto &j:graph[i])
+        {
+            if(!vis[j])
+            {
+                dfs(src, j, vis, graph, ans);
+            }
+        }
+    }
+    
     vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        //Create Graph and store indegree..
-        vector<int> indegree(n, 0);
         vector<vector<int>> graph(n);
-        for(auto &it:prerequisites)
+        vector<vector<int>> ans(n, vector<int> (n, 0));
+        for(auto &it:prerequisites) 
         {
             graph[it[0]].push_back(it[1]);
-            indegree[it[1]]++;
+            ans[it[1]][it[0]] = 1;
         }
         
-        queue<int> q;
-        for(int i=0; i<n; i++)  
-        {
-            if(indegree[i] == 0)    q.push(i);
-        }
+        //ans[j][i] = 1 if 'j' can be reachable by 'i'..
         
-        
-        //ans[j] will store nodes which can reach 'j'..
-        vector<vector<int>> ans(n, vector<int> (n, 0));
-        //Apply Bfs..
-        while(!q.empty())
+        for(int i=0; i<n; i++)
         {
-            int node = q.front();   q.pop();
-            
-            for(auto &j:graph[node])
-            {
-                indegree[j]--;
-                if(indegree[j] == 0)    q.push(j);
-                
-                ans[j][node] = 1;    //'node' can reach 'j'..
-                //if 'j' is reachable by 'node', then 'j' is also reachable by 'ans[node]'..
-                for(int x=0; x<n; x++)
-                {
-                    if(ans[node][x] == 1)
-                        ans[j][x] = 1;
-                }
-            }
+            vector<int> vis(n, 0);
+            dfs(i, i, vis, graph, ans);
         }
         
         //que[a, b], check if 'b' is reachable by 'a'..
