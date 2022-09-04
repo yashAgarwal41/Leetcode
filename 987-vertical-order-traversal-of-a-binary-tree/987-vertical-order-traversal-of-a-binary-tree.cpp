d242adca164d65cdcabe07205e41e0ff46cbe8f5
Yash Agarwal <agarwali4411@gmail.com>
2022-09-04 18:49:39 +0530

    Time: 6 ms (74.10%), Space: 12.4 MB (78.96%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,29 +11,40 @@
  */
 class Solution {
 public:
-    //DFS, TC-O(n*logn), SC-O(n)
-    map<int, map<int, multiset<int>>> map; //map -> [x -> {y-> nodes}]
-    void dfs(TreeNode* root, int index, int depth)
-    {
-        map[index][depth].insert(root->val);
-        
-        if(root->left)  dfs(root->left, index-1, depth+1); //left
-        if(root->right) dfs(root->right, index+1, depth+1); //right
-    }
+    //BFS, TC-O(nlogn), SC-O(n)
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        dfs(root, 0, 0);
-        vector<vector<int>> res;
+        map<int, map<int, multiset<int>>> map;
+         //map -> [vertical Index(x) -> {depth or level(y) -> nodes}]
+        queue<pair<TreeNode*, pair<int, int>>> q;   
+        //q-> [node, {vertical index(x), depth or level(y)}];
+        q.push({root, {0, 0}});
         
-        for(auto &x:map)    //index of vertical answer..
+        while(!q.empty())
+        {
+            TreeNode* node = q.front().first;
+            int x = q.front().second.first;
+            int y = q.front().second.second;
+            q.pop();
+            map[x][y].insert(node->val);
+            
+            if(node->left) q.push({node->left, {x-1, y+1}});    //left
+            if(node->right) q.push({node->right, {x+1, y+1}});  //right
+        }
+        
+        vector<vector<int>> res;
+        for(auto &x: map)     //index of vertical column..
         {
             vector<int> v;
-            for(auto &y:x.second)   //depth index(level in tree)..
+            for(auto &y: x.second)      //index of depth or level of tree....
             {
-                for(auto &node:y.second)
+                for(auto &node: y.second)   //all the falling nodes in map[x][y]..
+                {
                     v.push_back(node);
+                }
             }
             res.push_back(v);
         }
+        
         return res;
     }
 };
