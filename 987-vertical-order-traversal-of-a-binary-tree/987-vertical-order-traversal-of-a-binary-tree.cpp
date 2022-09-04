afb752408ce30295a9d36577f2664d5f14f33ad9
Yash Agarwal <agarwali4411@gmail.com>
2022-09-04 18:37:23 +0530

    Time: 3 ms (93.93%), Space: 14.8 MB (11.33%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,37 +11,27 @@
  */
 class Solution {
 public:
-    int mini = 1000, maxi = 1000, maxDepth=0;
-    void dfs(TreeNode* root, int index, int depth, vector<vector<multiset<int>>> &ans)
+    //DFS, TC-O(n*logn), SC-O(n)
+    map<int, map<int, multiset<int>>> map; //map -> [x -> {y-> nodes}]
+    void dfs(TreeNode* root, int index, int depth)
     {
-        mini = min(mini, index);
-        maxi = max(maxi, index);
-        maxDepth = max(maxDepth, depth);
-        ans[index][depth].insert(root->val);
+        map[index][depth].insert(root->val);
         
-        if(root->left)  dfs(root->left, index-1, depth+1, ans);
-        if(root->right) dfs(root->right, index+1, depth+1, ans);
+        if(root->left)  dfs(root->left, index-1, depth+1); //left
+        if(root->right) dfs(root->right, index+1, depth+1); //right
     }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<vector<multiset<int>>> ans(2005, vector<multiset<int>> (1005));
-        dfs(root, 1000, 0, ans);
+        dfs(root, 0, 0);
         vector<vector<int>> res;
         
-        for(int i=mini; i<=maxi; i++)
+        for(auto &x:map)    //index of vertical answer..
         {
             vector<int> v;
-            for(int j=0; j<=maxDepth; j++)
+            for(auto &y:x.second)   //depth index(level in tree)..
             {
-                if(ans[i][j].size()==0) continue;
-                for(auto &k:ans[i][j])
-                {
-                    v.push_back(k);
-                }
+                for(auto &node:y.second)
+                    v.push_back(node);
             }
-            // for(auto j : ans[i])
-            // {
-            //     v.push_back(j);
-            // }
             res.push_back(v);
         }
         return res;
