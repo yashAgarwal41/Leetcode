13b3850bf082a39064acca20c4e6ed315637072f
Yash Agarwal <agarwali4411@gmail.com>
2022-09-07 01:48:20 +0530

    Time: 47 ms (24.53%), Space: 13.5 MB (74.13%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,50 +1,37 @@
 class Solution {
 public:
-    //DFS..TC-O(n+e)
-    //if detect cycle, return empty array
-    //else return topological sort in reverse order..
-    bool isCycleDfs(int i, vector<int> &vis, vector<int> &dfsVis, stack<int> &st, vector<set<int>> &graph)
-    {
-        vis[i]=1;
-        dfsVis[i]=1;
-        for(auto &j:graph[i])
-        {
-            if(!vis[j])
-            {
-                if(isCycleDfs(j, vis, dfsVis, st, graph))   //if detect cycle
-                    return true;
-            }
-            else if(dfsVis[j])  //if detect cycle
-                return true;
-        }
-        st.push(i); //topological sort
-        dfsVis[i]=0;
-        return false;   //no cycle detected
-    }
+    //BFS, topo sort, TC-(n+e)..
+    //[u, v]-> to finsih 'u' we should perform 'v' first, that means v->u
+    //so in topo sort.. 'v' comes before 'u'..
     vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
-        vector<set<int>> graph(n);
-        vector<int> vis(n, 0), dfsVis(n, 0);
-        vector<int> res;
-        stack<int> st;
-        for(auto &v:prerequisites)
+        vector<vector<int>> graph(n);
+        vector<int> indegree(n, 0);
+        for(auto &it:prerequisites)
         {
-            graph[v[0]].insert(v[1]);
+            //v->u, 'v' shoould be perform before 'u'..
+            graph[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
+        }
+        queue<int> q;
+        for(int i=0; i<n; i++)  
+        {
+            if(indegree[i]==0)  q.push(i);
         }
         
-        for(int i=0; i<n; i++)
+        vector<int> topo;
+        while(!q.empty())
         {
-            if(!vis[i])
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto &j:graph[node])
             {
-                if(isCycleDfs(i, vis, dfsVis, st, graph))
-                    return res;
+                indegree[j]--;
+                if(indegree[j]==0)  q.push(j);
             }
         }
-        while(!st.empty())
-        {
-            res.push_back(st.top());
-            st.pop();
-        }
-        reverse(res.begin(), res.end());
-        return res;
+        
+        if(topo.size()==n)  return topo;
+        else return {};
     }
 };
