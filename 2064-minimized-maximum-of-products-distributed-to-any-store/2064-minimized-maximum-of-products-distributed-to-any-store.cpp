0d172b91221d8c8d32a8f00c4375440dca93a26c
Yash Agarwal <agarwali4411@gmail.com>
2022-09-08 01:22:34 +0530

    Time: 526 ms (16.36%), Space: 84.4 MB (81.42%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     //Binary Search, TC-O(nlogn)..
-    //high = 1e10, because e.g, n=1, m=1e5, v=[1e5, 1e5, ....1e5], then we need to distribute all '1e10' products to the only shop..
+    //high = 1e5, because e.g, n=1, m=1, v=[1e5], then we need to distribute all '1e5' products to the only shop..
     int minimizedMaximum(int n, vector<int>& v) {
         int m = v.size();
-        long low = 1, high = 1e10, ans;
+        int low = 1, high = 1e5, ans;
         
         while(low<=high)
         {
-            long mid = low + (high-low)/2;
-            long totalShops = 0;    //totalShops that can hold the quantity 'mid'..
+            int mid = low + (high-low)/2;
+            int totalShops = 0;    //totalShops that can hold the quantity 'mid'..
             for(auto &i : v)
             {
                 totalShops += ceil((1.0*i)/mid);
