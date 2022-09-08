3b9938706a1c93fe7a3f3e41fd605c8bb38f1248
Yash Agarwal <agarwali4411@gmail.com>
2022-09-08 21:57:40 +0530

    Time: 688 ms (13.22%), Space: 123.3 MB (25.08%) - LeetHub

--- 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
+++ 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
+    //LIS(Binary Search), TC_O(nlogn)..
     vector<int> longestObstacleCourseAtEachPosition(vector<int>& v) {
         int n = v.size();
         vector<int> res, lis;
         for(int i=0; i<n; i++)
         {
-            auto lb = lower_bound(lis.begin(), lis.end(), v[i]);
+            int ind = upper_bound(lis.begin(), lis.end(), v[i]) - lis.begin();
             
-            if(lb == lis.end())
+            if(ind == lis.size())
             {
                 res.push_back(lis.size()+1);
                 lis.push_back(v[i]);
             }
             else
             {
-                if(*lb == v[i])
-                {
-                    auto ub = upper_bound(lis.begin(), lis.end(), v[i]);
-                    res.push_back(ub-lis.begin()+1);
-                    if(ub!=lis.end())   *ub = v[i];
-                    else lis.push_back(v[i]);
-                }
-                else 
-                {
-                    res.push_back(lb-lis.begin()+1);
-                    *lb = v[i];
-                }
+                res.push_back(ind+1);
+                lis[ind] = v[i];
             }
-            // for(auto j:lis) cout<<j<<" ";
-            // cout<<endl;
         }
         return res;
     }
