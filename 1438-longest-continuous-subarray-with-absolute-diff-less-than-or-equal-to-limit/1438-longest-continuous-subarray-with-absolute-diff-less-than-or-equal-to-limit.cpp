f047f9b62a37acc20208fd45cd84bc850667c9f1
Yash Agarwal <agarwali4411@gmail.com>
2022-09-08 23:33:30 +0530

    Time: 325 ms (27.19%), Space: 62.8 MB (38.50%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
+    //sliding window + multiset, TC-O(nlogn)
     int longestSubarray(vector<int>& v, int limit) {
         int n = v.size(), maxi=1;
-        multiset<int> set;
+        map<int, int> map;
         int i=0, j=0;
         
         while(i<n and j<n)
         {
-            set.insert(v[j]);
+            map[v[j]]++;
             int low = v[j]-limit, high = v[j]+limit;
             while(true)
             {
-                if(set.size()==0)   break;
-                auto beg = set.begin();
-                auto en = set.end();
+                if(map.size()==0)   break;
+                auto beg = map.begin();
+                auto en = map.end();
                 en--;
-                if(*beg >= low and *en <= high)  break;
-                set.erase(set.find(v[i]));
+                if(beg->first >= low and en->first <= high)  break;
+                map[v[i]]--;
+                if(map[v[i]]==0)    map.erase(v[i]);
                 i++;
             }
-            // cout<<"i: "<<i<<", j: "<<j<<" -> ";
-            // for(auto it:set)    cout<<it<<" ";
-            // cout<<endl;
             maxi = max(maxi, j-i+1);
             j++;
         }
