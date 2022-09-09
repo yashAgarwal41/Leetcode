72d53e265d04ebb4408d8c66a358993bfcd7970a
Yash Agarwal <agarwali4411@gmail.com>
2022-09-09 17:05:51 +0530

    Time: 510 ms (20.77%), Space: 102.1 MB (66.72%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
+    //Memoization, TC-O(n)..
     int n;
     int dp[100001][2];
     int help(int i, bool swaped, vector<int> &v1, vector<int> &v2)
     {
         if(i==n)    return 0;
         if(dp[i][swaped]!=-1)   return dp[i][swaped];
+        
+        int ans = 1e6;
         if(i==0)
         {
-            int ans = min(help(i+1, 0, v1, v2), 1 + help(i+1, 1, v1, v2));
-            return ans;
+            ans = min(help(i+1, 0, v1, v2), 1 + help(i+1, 1, v1, v2));
+            return dp[i][swaped] = ans;
         }
         
         int prev1 = v1[i-1], prev2 = v2[i-1];
         int curr1 = v1[i], curr2 = v2[i];
-        if(swaped == true)   swap(prev1, prev2);
+        if(swaped == true)   swap(prev1, prev2);    //if previous indices are swaped..
         
-        int ans = 1e6;
+        //if already increasing, then no need to swap current indices..
         if(prev1 < curr1 and prev2 < curr2)
         {
             ans = min(ans, help(i+1, 0, v1, v2));
         }
+        //if also increasing on swapping current indices, then swap currents and check if it give minimum..
         if(prev1 < curr2 and prev2 < curr1)
         {
             ans = min(ans, 1 + help(i+1, 1, v1, v2));
