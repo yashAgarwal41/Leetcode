a4d039b2d6bb89b5e4957ef851fa57ffc9f6a88d
Yash Agarwal <agarwali4411@gmail.com>
2022-09-24 10:31:38 +0530

    Time: 23 ms (47.01%), Space: 19.9 MB (60.91%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -13,21 +13,24 @@ class Solution {
 public:
     void dfs(TreeNode* root, int sum, int target, vector<int> &path, vector<vector<int>> &res)
     {
-
-        sum += root->val;
-        path.push_back(root->val);
         if(root->left == NULL and root->right == NULL)
         {
-            if(sum == target)   res.push_back(path);
-            sum -= root->val;
-            path.pop_back();
+            if(sum + root->val == target)   
+            {
+                path.push_back(root->val);
+                res.push_back(path);
+                path.pop_back();
+            }
             return;
         }
         
+        sum += root->val;
+        path.push_back(root->val);
         if(root->left)  dfs(root->left, sum, target, path, res);
-        if(root->right) dfs(root->right, sum, target, path, res);
+        if(root->right)  dfs(root->right, sum, target, path, res);
         sum -= root->val;
         path.pop_back();
+        
     }
     vector<vector<int>> pathSum(TreeNode* root, int target) {
         vector<vector<int>> res;
