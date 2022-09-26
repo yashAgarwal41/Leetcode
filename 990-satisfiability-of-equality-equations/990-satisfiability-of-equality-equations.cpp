502b30f96ea48406af4aa71529a50ee0b075c75a
Yash Agarwal <agarwali4411@gmail.com>
2022-09-26 18:36:27 +0530

    Time: 20 ms (13.29%), Space: 12.8 MB (8.67%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    //DFS-
+    
     void dfs(char parent, char node, unordered_map<char, set<char>> &equal, vector<int> &vis)
     {
         vis[node - 'a'] = 1;
@@ -13,30 +15,25 @@ public:
         }
     }
     bool equationsPossible(vector<string>& v) {
-        unordered_map<char, set<char>> equal;
+        unordered_map<char, set<char>> equal;   //contains only equal elements..
         for(auto &str:v)
         {
             if(str[1]=='=')
             {
-                equal[str[0]].insert(str[3]);
-                equal[str[3]].insert(str[0]);
-                equal[str[0]].insert(str[0]);
-                equal[str[3]].insert(str[3]);
+                equal[str[0]].insert(str[3]);   //a->b
+                equal[str[3]].insert(str[0]);   //b->a
+                equal[str[0]].insert(str[0]);   //a->a
+                equal[str[3]].insert(str[3]);   //b->b
             }
             else if(str[0] == str[3])   return false;
         }
         
+        //push all elements which are indirectly equal to each other
         for(auto it:equal)
         {
             vector<int> vis(26, 0);
-            dfs(it.first, it.first, equal, vis);
+            dfs(it.first, it.first, equal, vis);    
         }
-        // for(auto it:equal)
-        // {
-        //     cout<<it.first<<"->";                 
-        //     for(auto j:it.second)   cout<<j<<" ";
-        //     cout<<endl;
-        // }
         for(auto &str:v)
         {
             if(str[1]=='!')
