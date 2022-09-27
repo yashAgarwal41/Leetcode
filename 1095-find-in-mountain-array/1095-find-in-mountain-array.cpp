af1088c3579cbca7e8edf5be3a878582859fc7b6
Yash Agarwal <agarwali4411@gmail.com>
2022-09-27 21:06:12 +0530

    Time: 12 ms (18.46%), Space: 7 MB (53.85%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -12,8 +12,8 @@ class Solution {
 public:
     //Binary Search, TC-(logn)..
     //find peak element and then search for target in 2 different regions..
-    int findInMountainArray(int target, MountainArray &mountainArr) {
-        int n = mountainArr.length(), mini=INT_MAX;
+    int findPeak(int n, MountainArray &mountainArr)
+    {
         int low = 0, high = n-1, peak;
         while(low<=high)
         {
@@ -32,10 +32,10 @@ public:
             else if(mid==0) low++;
             else high--;    //if(mid==n-1)
         }
-        cout<<peak<<endl;
-        if(mountainArr.get(peak) == target) return peak;
-        
-        low = 0, high = peak-1;
+        return peak;
+    }
+    int findInLeft(int low, int high, int target, MountainArray &mountainArr)
+    {
         while(low<=high)
         {
             int mid = low+(high-low)/2;
@@ -44,16 +44,30 @@ public:
             if(val>target)  high = mid-1;
             else low = mid+1;
         }
-        low = peak+1, high = n-1;
+        return -1;
+    }
+    int findInRight(int low, int high, int target, MountainArray &mountainArr)
+    {
         while(low<=high)
         {
             int mid = low+(high-low)/2;
             int val = mountainArr.get(mid);
             if(val == target)   return mid;
             if(val>target)  low = mid+1;
-            else high = mid-1;
+            else high= mid-1;
         }
-    
         return -1;
+    }
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int n = mountainArr.length(), mini=INT_MAX;
+        
+        int peak = findPeak(n, mountainArr);
+        if(mountainArr.get(peak) == target) return peak;
+        cout<<peak<<endl;
+        int left = findInLeft(0, peak-1, target, mountainArr);
+        if(left!=-1)  return left;
+        int right = findInRight(peak+1, n-1, target, mountainArr);
+        return right;
+        
     }
 };
