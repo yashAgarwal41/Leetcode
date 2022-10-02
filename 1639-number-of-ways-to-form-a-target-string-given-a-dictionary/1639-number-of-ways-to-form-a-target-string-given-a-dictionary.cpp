1363a05ba48d10d40eba19811b37d03852aa9ef2
Yash Agarwal <agarwali4411@gmail.com>
2022-10-02 11:52:36 +0530

    Time: 676 ms (25.28%), Space: 32.7 MB (73.64%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -4,7 +4,7 @@ public:
     int n, m;
     int dp[1001][1001];
     int mod = 1e9+7;
-    int help(int i, int col, string &target, vector<unordered_map<char, int>> &freq)
+    int help(int i, int col, string &target, vector<vector<int>> &freq)
     {
         if(i>=target.size())    return 1;
         if(col>=n)  return 0;
@@ -12,11 +12,10 @@ public:
         long long pick=0, notPick=0;
         
         char c = target[i];
-        if(freq[col].count(c))
+        if(freq[col][c-'a']>0)
         {
-            pick = (pick + 1LL*freq[col][c]*help(i+1, col+1, target, freq))%mod;
+            pick = (pick + 1LL*freq[col][c-'a']*help(i+1, col+1, target, freq))%mod;
         }
-        
         notPick = help(i, col+1, target, freq)%mod;
         
         return dp[i][col] = (pick + notPick + 0LL)%mod;
@@ -25,13 +24,13 @@ public:
         n = words[0].size(), m = target.size();
         memset(dp, -1, sizeof dp);
         
-        vector<unordered_map<char, int>> freq(n);
+        vector<vector<int>> freq (n, vector<int>(26, 0));
         for(int j=0; j<words[0].size(); j++)
         {
             for(int i=0; i<words.size(); i++)
             {
                 char c = words[i][j];
-                freq[j][c]++;
+                freq[j][c-'a']++;
             }
         }
         return help(0, 0, target, freq);
