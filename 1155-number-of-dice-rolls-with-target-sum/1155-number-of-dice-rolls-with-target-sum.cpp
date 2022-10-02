b6e3ac4c449299b55f2e833af2ebc7ddd762da22
Yash Agarwal <agarwali4411@gmail.com>
2022-10-02 11:03:47 +0530

    Time: 259 ms (5.19%), Space: 8.6 MB (28.55%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    //Memoization..TC-O(n*k*target)
+    //Bottom Up DP..TC-O(n*target*k)
     int mod = 1e9+7;
-    int dp[31][1001];
-    int help(int i, int target, int &k)
-    {
-        if(i==0 and target==0)  return 1;
-        if(i==0 or target<=0)    return 0;
-        if(dp[i][target] != -1)   return dp[i][target];
-        
-        long long ans = 0;
-        for(int j=1; j<=k; j++)
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>> dp(n+1, vector<int> (target+1, 0));
+        dp[0][0] = 1;
+        for(int i=1; i<=n; i++)
         {
-            if(j>target)    break;
-            ans = (ans + help(i-1, target-j, k))%mod;
+            for(int tar=1; tar<=target; tar++)
+            {
+                for(int face=1; face<=k; face++)
+                {
+                    if(face > tar)  break;
+                    dp[i][tar] = (dp[i][tar]%mod + dp[i-1][tar-face]%mod)%mod;
+                }
+             }
         }
-        return dp[i][target] = ans;
-    }
-    int numRollsToTarget(int n, int k, int target) 
-    {
-        memset(dp, -1, sizeof dp);
-        return help(n, target, k);
+        return dp[n][target]%mod;
     }
 };
