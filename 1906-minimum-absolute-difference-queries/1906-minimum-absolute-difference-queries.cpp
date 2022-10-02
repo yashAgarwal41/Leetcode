4edcb91708d7e91eb64120df6e9a22129406766a
Yash Agarwal <agarwali4411@gmail.com>
2022-10-02 21:27:18 +0530

    Time: 975 ms (71.63%), Space: 228.2 MB (41.14%) - LeetHub

--- 1906-minimum-absolute-difference-queries/1906-minimum-absolute-difference-queries.cpp
+++ 1906-minimum-absolute-difference-queries/1906-minimum-absolute-difference-queries.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
-    //TC-O(q*100*log(n))
-    void markPresentValues(int l, int r, vector<int> &freq, unordered_map<int, set<int>> &map)
+    //Prefix Number, TC-O(q*100)
+    void markPresentValues(int n, int l, int r, vector<int> &freq, int pS[n+2][101])
     {
         for(int j=1; j<=100; j++)
         {
-            auto lb = map[j].lower_bound(l);
-            if(lb == map[j].end())  continue;
-            if(*lb <=r )    freq.push_back(j);
+            int found = pS[r][j];
+            if(l>0) found -= pS[l-1][j];
+            if(found>0) freq.push_back(j);
+            
         }
     }
     vector<int> minDifference(vector<int>& v, vector<vector<int>>& que) {
-        unordered_map<int , set<int>> map;
-        for(int i=0; i<v.size(); i++)   map[v[i]].insert(i);
+        int n = v.size();
+        
+        //store the prefix of frequency of 1<=j<=100  
+        int pS[n+2][101];
+        memset(pS, 0, sizeof pS);
+        pS[0][v[0]]++;
+        for(int i=1; i<n; i++)
+        {
+            pS[i][v[i]] = 1;
+            for(int j=1; j<=100; j++)
+            {
+                pS[i][j] += pS[i-1][j];
+            }
+        }
         
         vector<int> res;
         for(auto &q:que)
         {
             vector<int> freq;
-            markPresentValues(q[0], q[1], freq, map);
+            markPresentValues(n, q[0], q[1], freq, pS);
             
             //check minimum absolute difference..
             int mini = 101;
