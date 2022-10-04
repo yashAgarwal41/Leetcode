e2563cee6685a4abb2dd0e6a2c6655839c5f4720
Yash Agarwal <agarwali4411@gmail.com>
2022-10-05 00:26:39 +0530

    Time: 126 ms (13.12%), Space: 24.5 MB (13.91%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     int findTheWinner(int n, int k) {
         if(n==1)    return 1;
-        vector<int> v;
-        for(int i=1; i<=n; i++) v.push_back(i);
+        queue<int> q;
+        for(int i=1; i<=n; i++) q.push(i);
         
         int start_ind = 0;
-        while(v.size()>1)
+        while(q.size()>1)
         {
-            n = v.size();
-            int stop_ind = (start_ind + k - 1)%n;
-            v.erase(v.begin() + stop_ind);
-            start_ind = (stop_ind)%n;
+            int step = k;
+            while(step>1)
+            {
+                int curr = q.front();   //the 1st friend of circle..
+                q.pop();    //remove it and shift to last of circle..
+                q.push(curr);   //shifted..
+                step--;
+            }
+            q.pop();    //remove the kth step friend from game..
         }
         
-        return v[0];
+        return q.front();
     }
 };
