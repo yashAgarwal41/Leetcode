86e2a2dc9d140bffbb0d4d45779144766418d7cb
Yash Agarwal <agarwali4411@gmail.com>
2022-10-05 22:11:49 +0530

    Time: 698 ms (9.33%), Space: 112.8 MB (14.52%) - LeetHub

--- 1537-get-the-maximum-score/1537-get-the-maximum-score.cpp
+++ 1537-get-the-maximum-score/1537-get-the-maximum-score.cpp
@@ -7,23 +7,25 @@ public:
     int maxi = 0;
     long long help(int i, int path, vector<int> &v1, vector<int> &v2)
     {
-        if(i>=n and i>=m)    return 0;
+        // if(i>=n and i>=m)    return 0;
+        if(path == 1 and i>=n)   return 0;
+        if(path == 2 and i>=m)  return 0;
         if(dp[i][path]!=-1) return dp[i][path];
         
         long long samePath = 0, differentPath = 0;
         
         //continue with current path/array..
-        if(path == 1 and i<n)   samePath = (v1[i] + help(i+1, path, v1, v2) + 0LL);
-        if(path == 2 and i<m)   samePath = (v2[i] + help(i+1, path, v1, v2) + 0LL);
+        if(path == 1)   samePath = (v1[i] + help(i+1, path, v1, v2) + 0LL);
+        if(path == 2)   samePath = (v2[i] + help(i+1, path, v1, v2) + 0LL);
         
         //change the path/array..
-        if(path == 1 and i<n)
+        if(path == 1)
         {
             int val = v1[i];
             if(map2.find(val) != map2.end())   
                 differentPath = (val + help(map2[val]+1, 2, v1, v2) + 0LL);
         }
-        else if(path == 2 and i<m)
+        else if(path == 2)
         {
             int val = v2[i];
             if(map1.find(val) != map1.end())
@@ -34,7 +36,7 @@ public:
     }
     int maxSum(vector<int>& v1, vector<int>& v2) {
         n = v1.size(), m = v2.size();
-        // cout<<n<<" "<<m<<endl;
+        
         for(int i=0; i<n; i++)  map1[v1[i]] = i;
         for(int i=0; i<m; i++)  map2[v2[i]] = i;
         memset(dp, -1, sizeof dp);
