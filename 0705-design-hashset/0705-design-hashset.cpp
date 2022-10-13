440d54cdc5feb5595697633ed980c3f04b8fb947
Yash Agarwal <agarwali4411@gmail.com>
2022-10-13 15:35:28 +0530

    Time: 136 ms (80.24%), Space: 41 MB (83.39%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -0,0 +1,39 @@
+class MyHashSet {
+public:
+    int n;
+    vector<vector<int>> v;
+    MyHashSet() {
+        n = 1000;
+        v.resize(n);
+    }
+    
+    int hash(int key){
+        return key%n;
+    }
+    void add(int key) {
+        int i = hash(key);
+        auto it = find(v[i].begin(), v[i].end(), key);
+        if(it == v[i].end())    v[i].push_back(key);
+    }
+    
+    void remove(int key) {
+        int i = hash(key);
+        auto it = find(v[i].begin(), v[i].end(), key);
+        if(it != v[i].end())    v[i].erase(it);
+    }
+    
+    bool contains(int key) {
+        int i = hash(key);
+        auto it = find(v[i].begin(), v[i].end(), key);
+        if(it != v[i].end())    return true;
+        else return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
