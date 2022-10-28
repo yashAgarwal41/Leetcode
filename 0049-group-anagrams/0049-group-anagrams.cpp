065d639410c097d79748fe6a371b2a14652ed7ea
Yash Agarwal <agarwali4411@gmail.com>
2022-10-28 11:57:54 +0530

    Time: 91 ms (30.16%), Space: 23 MB (19.49%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     //TC-O(n*100)
+    string countAndSort(string &str)
+    {
+        vector<int> freq(26, 0);
+        for(auto &c:str)    freq[c-'a']++;
+        
+        string s = "";
+        for(int i=0; i<26; i++)
+        {
+            while(freq[i]--)    s += (i+'a');
+        }
+        return s;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n = strs.size();
         vector<vector<string>> res;
-        map<map<char, int>, vector<string>> mmap;
+        unordered_map<string, vector<string>> mmap;
         
         for(auto &str:strs)
         {
-            map<char, int> freq;
-            for(auto &c:str)
-            {
-                freq[c]++;
-            }
-            mmap[freq].push_back(str);
+            string s = countAndSort(str);
+            mmap[s].push_back(str);
         }
         for(auto &v:mmap)    res.push_back(v.second);
         return res;
