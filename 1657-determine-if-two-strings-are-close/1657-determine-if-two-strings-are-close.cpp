bf5f2c1baf2d88d125477f94a59960ebcdc40a38
Yash Agarwal <agarwali4411@gmail.com>
2022-12-02 13:19:39 +0530

    Time: 100 ms (88.92%), Space: 20.4 MB (82.93%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
+    //TC-O(n)..
     bool closeStrings(string word1, string word2) {
-        int n = word1.size(), m = word2.size();
-        if(n!=m)    return false;
-        vector<int> freq1(26), freq2(26);
-        for(auto &c:word1)  freq1[c-'a']++;
-        for(auto &c:word2)  freq2[c-'a']++;
+        if(word1.size()!=word2.size())  return false;
         
-        map<int, int> map1, map2;
-        for(int i=0; i<26; i++)
+        vector<int> freq1(26), freq2(26);
+        vector<int> found1(26), found2(26);
+        for(auto &c:word1)
         {
-            if(freq1[i]>0 and freq2[i]==0 or freq1[i]==0 and freq2[i]>0)  return false;
-            map1[freq1[i]]++;
-            map2[freq2[i]]++;
+            freq1[c-'a']++;
+            found1[c-'a'] = 1;
         }
-        if(map1==map2)  return true;
-        else return false;
+        for(auto &c:word2)
+        {
+            freq2[c-'a']++;
+            found2[c-'a'] = 1;
+        }
+        sort(freq1.begin(), freq1.end());   //TC-O(26log(26))
+        sort(freq2.begin(), freq2.end());
+        return freq1==freq2 and found1 == found2;
     }
 };
-
-// a-1, b-1, c-1
-// a-1, b-1, c-1
-
-//a-2, b-3, c-1
-//a-1, b-2, c-3
-
-// a-3, b-4, c-2, d-2, e-5, f-5
-// a-6, b-2, c-3, d-3, e-4, f-4
