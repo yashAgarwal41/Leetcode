4df77a1d538c51cc5cf19bd3f17ee8efa616bfe7
Yash Agarwal <agarwali4411@gmail.com>
2023-01-12 22:43:55 +0530

    Time: 704 ms (71.85%), Space: 243.7 MB (24.45%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    //dfs
+    vector<int> res;
+    vector<int> dfs(int i, string &labels, vector<int> &vis, vector<set<int>> &graph)
+    {
+        vis[i] = 1;
+        vector<int> freq(26, 0);
+        freq[labels[i] - 'a'] = 1;
+        for(auto &j:graph[i])
+        {
+            if(!vis[j])
+            {
+                vector<int> temp = dfs(j, labels, vis, graph);
+                for(int k = 0; k<26; k++)
+                    freq[k] += temp[k];
+            }
+        }
+        res[i] += freq[labels[i] - 'a'];
+        return freq;
+    }
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        vector<set<int>> graph(n);
+        for(auto &v:edges)
+        {
+            graph[v[0]].insert(v[1]);
+            graph[v[1]].insert(v[0]);
+        }
+        vector<int> vis(n, 0);
+        res.resize(n, 0);
+        dfs(0, labels, vis, graph);
+        return res;
+    }
+};
