9077dc36d5b8aa517a8191d6dc8c82ecd13c2e43
Yash Agarwal <agarwali4411@gmail.com>
2023-11-02 14:56:05 +0530

    Time: 44 ms (91.10%), Space: 40.9 MB (99.53%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: (value)=>{
+            if(val === value)   return true;
+            throw new Error("Not Equal");
+        },
+        notToBe: (value)=>{
+            if(val !== value)    return true;
+            throw new Error("Equal");    
+        } 
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
