711140e0bc2eaf88427f0d0960bb4fa57620fe5c
Yash Agarwal <agarwali4411@gmail.com>
2023-11-10 16:03:45 +0530

    Time: 83 ms (69.18%), Space: 63.5 MB (56.83%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int getWinner(vector<int>& v, int k) {
-        int n = v.size();
-        int maxi = *max_element(v.begin(), v.end());
-        unordered_map<int, int> map1;
+        int n = v.size(), cnt=0;
         for(int i=0; i<n-1; i++)
         {
-            if(v[i] > v[i+1])   swap(v[i], v[i+1]);
-            map1[v[i+1]]++;
-            if(map1[v[i+1]] == k)   return v[i+1];
+            if(v[i] > v[i+1])
+            {
+                swap(v[i], v[i+1]);
+                cnt++;
+            }
+            else cnt=1;
+            if(cnt == k)   return v[i+1];
         }
-        return maxi;
+        return *max_element(v.begin(), v.end());
     }
 };
